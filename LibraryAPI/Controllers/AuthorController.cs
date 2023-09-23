e644c0c87354af63460d00f0674b4548415aeb48
Aslannymous <aslanhuseynov9@gmail.com>
2023-09-24 03:51:32 +0400

    start project with clean architecture

---
+++ LibraryAPI/Controllers/AuthorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+    }
+}
